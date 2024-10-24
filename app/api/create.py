1056d0ce48eff7a361f71962107004f6a42e52e6
Semuca <pokelock@icloud.com>
2024-10-24 09:49:43 +0800

    feat: (mostly working) add entities

--- app/api/create.py
+++ app/api/create.py
@@ -1,8 +1,26 @@
 """Defines endpoint for creating entries"""
 
+from flask import request
+from rdflib import RDF, RDFS, Literal, URIRef
 from .bp import api_bp
+from main.views import g, domain
 
 @api_bp.route('/', methods=['POST'])
 def create_entry():
     """Create an entry"""
+
+    body = request.get_json()
+    rdf_type = body.get('rdf_type')
+    label = body.get('label')
+
+    if not rdf_type or not label:
+        return 'RDF type or label not provided', 400
+    
+    uri = URIRef(f'{domain}{rdf_type.lower()}/{label}')
+
+    g.add((uri, RDF.type, URIRef(f'{domain}properties/{rdf_type.capitalize()}')))
+    g.add((uri, RDFS.label, Literal(label)))
+
+    g.serialize('../graph.ttl', format='turtle')
+
     return 'Entry created', 201
--- app/static/js/add.js
+++ app/static/js/add.js
@@ -1,18 +1,31 @@
 const addButton = document.getElementById("addButton");
 addButton.addEventListener("click", () => {
-    //TODO: Implement add functionality
-    
+
+    const tabContainer = document.getElementById("add-tabs");
+    const selectedTabWindow = tabContainer.querySelector('.tab-content.active');
+
+    const rdf_type = selectedTabWindow.getAttribute("data-rdf-type");
+
+    const searchInput = selectedTabWindow.querySelector('#searchInput');
+    const label = searchInput.value ? searchInput.value : null;
+
     fetch('/api/', {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            rdf_type: rdf_type,
+            label: label,
+        })
     }).then(response => {
         if (!response.ok) {
             throw new Error(response.statusText);
         }
         return response.text();
     }).then(data => {
-        console.log(data);
+        location.reload();
     }).catch(error => {
         console.error(error);
     });
-    // window.location.href = '/';
 });
