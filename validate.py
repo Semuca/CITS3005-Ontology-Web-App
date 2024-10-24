20527f9c74717ea59bf2b07901e65147cdba1afc
Semuca <pokelock@icloud.com>
2024-10-24 19:14:40 +0800

    feat: use reasoner with validator

--- validate.py
+++ validate.py
@@ -1,9 +1,11 @@
 
 from rdflib import Graph
 from pyshacl import validate
-from owlready2 import get_ontology, default_world
+from owlready2 import get_ontology, default_world, sync_reasoner_pellet
 
 get_ontology("ontology.owl").load()
+sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True)
+
 knowledge_graph = default_world.as_rdflib_graph()
 
 shacl_graph = Graph()
