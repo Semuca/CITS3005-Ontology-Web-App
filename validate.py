4db7ad8e919db6d292b89908c6610b6ca67f2919
Semuca <pokelock@icloud.com>
2024-10-24 17:41:04 +0800

    feat: set up basic pyshacl verification

---
+++ validate.py
@@ -0,0 +1,19 @@
+
+from rdflib import Graph
+from pyshacl import validate
+from owlready2 import get_ontology, default_world
+
+get_ontology("ontology.owl").load()
+knowledge_graph = default_world.as_rdflib_graph()
+
+shacl_graph = Graph()
+shacl_graph.parse("shapes.ttl", format="turtle")
+
+conforms, results_graph, results_text = validate(
+    knowledge_graph,
+    shacl_graph=shacl_graph,
+    inference='none',
+    debug=True
+)
+
+print(results_text)
